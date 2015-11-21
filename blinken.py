89479c0020e24f20aa503fdb0ffb4b3e97c0ab33
Andy Zimolzak (from Raspberry Pi) <andyzimolzak@gmail.com>
2015-11-21 15:32:33 -0500

    Initial commit with wifi setup and blinkenlight code

---
+++ blinken.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+import RPi.GPIO as GPIO
+from time import sleep
+
+hertz = 10
+duration = 4
+
+nflashes = hertz * duration
+seconds_to_sleep = 1.0 / hertz / 2
+
+GPIO.setmode(GPIO.BOARD) # means, like, wire 11, not GPIO 11
+GPIO.setup(11, GPIO.OUT) # requires root?
+
+def onoff(period):
+    GPIO.output(11, GPIO.HIGH)
+    sleep(period)
+    GPIO.output(11, GPIO.LOW)
+    sleep(period)
+
+for i in range(nflashes):
+    onoff(seconds_to_sleep)
+
+GPIO.cleanup()
