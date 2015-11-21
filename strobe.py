ca06bf1d52cd51ccec178c98ad407bfe59f1ada1
Andy Zimolzak (from Raspberry Pi) <andyzimolzak@gmail.com>
2015-11-21 17:37:12 -0500

    Make onoff function more versatile

--- strobe.py
+++ strobe.py
@@ -1,17 +1,18 @@
+# Adapted from code by Rahul Kar
+# http://www.rpiblog.com/2012/09/using-gpio-of-raspberry-pi-to-blink-led.html
+
 import RPi.GPIO as GPIO
 from time import sleep
 
-def onoff(period, pin):
-    """Symmetric square wave, equal time on/off"""
-    half_cycle = period / 2.0
+def onoff(ontime, offtime, pin):
     GPIO.output(pin, GPIO.HIGH)
-    sleep(half_cycle)
+    sleep(ontime)
     GPIO.output(pin, GPIO.LOW)
-    sleep(half_cycle)
+    sleep(offtime)
 
 def strobe(freq, dur, pin):
     nflashes = freq * dur
-    seconds_to_sleep = 1.0 / freq
+    period = 1.0 / freq
 
     # Use Raspberry-Pi board pin numbers. In other words, 11 means pin
     # number 11, not GPIO 11.
@@ -19,5 +20,5 @@ def strobe(freq, dur, pin):
 
     GPIO.setup(pin, GPIO.OUT) # requires root?
     for i in range(nflashes):
-        onoff(seconds_to_sleep, pin)
+        onoff(period/2.0, period/2.0, pin)
     GPIO.cleanup()
