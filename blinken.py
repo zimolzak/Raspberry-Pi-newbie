56f1583627ae603e788b2f208214dad53e136956
Andy Zimolzak (from Raspberry Pi) <andyzimolzak@gmail.com>
2015-11-21 17:46:52 -0500

    Ability to use onoff() function outside of module.

--- blinken.py
+++ blinken.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python
 
-from strobe import strobe
+from strobe import strobe, setup, cleanup, onoff
 
 hertz = 0.5
 duration = 10
+pin = 11
 
-strobe(hertz, duration, 11)
+setup(pin)
+strobe(hertz, duration, pin)
+cleanup()
--- strobe.py
+++ strobe.py
@@ -9,16 +9,18 @@ def onoff(ontime, offtime, pin):
     sleep(ontime)
     GPIO.output(pin, GPIO.LOW)
     sleep(offtime)
+    
+def setup(pin):
+    # Use Raspberry-Pi board pin numbers. In other words, 11 means pin
+    # number 11, not GPIO 11.
+    GPIO.setmode(GPIO.BOARD) 
+    GPIO.setup(pin, GPIO.OUT) # requires root?
+
+def cleanup():
+    GPIO.cleanup()
 
 def strobe(freq, dur, pin):
     nflashes = int(freq * dur)
     period = 1.0 / freq
-
-    # Use Raspberry-Pi board pin numbers. In other words, 11 means pin
-    # number 11, not GPIO 11.
-    GPIO.setmode(GPIO.BOARD) 
-
-    GPIO.setup(pin, GPIO.OUT) # requires root?
     for i in range(nflashes):
         onoff(period/2.0, period/2.0, pin)
-    GPIO.cleanup()
