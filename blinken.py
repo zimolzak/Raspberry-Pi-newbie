583209ce74e8aeba1700a6a7df42465df7ae0ab8
Andy Zimolzak (from Raspberry Pi) <andyzimolzak@gmail.com>
2015-11-21 18:02:02 -0500

    Functions to turn .- string into morse letter

--- blinken.py
+++ blinken.py
@@ -1,25 +1,33 @@
 #!/usr/bin/env python
 
 from strobe import strobe, setup, cleanup, onoff
+from time import sleep
 
 pin = 11
 
-dash = 0.7
-pause = 0.15
-dot = 0.2
+dash_t = 0.7
+pause_t = 0.15
+dot_t = 0.2
+letter_t = 0.5
+
+def dash():
+    onoff(dash_t, pause_t, pin) # closure
+
+def dot():
+    onoff(dot_t, pause_t, pin)
+
+def letter(string):
+    for char in string:
+        if char == '-':
+            dash()
+        if char == '.':
+            dot()
+    sleep(letter_t)
 
 setup(pin)
 
-onoff(dot, pause, pin)
-onoff(dot, pause, pin)
-onoff(dot, pause + 0.5, pin)
-
-onoff(dash, pause, pin)
-onoff(dash, pause, pin)
-onoff(dash, pause + 0.5, pin)
-
-onoff(dot, pause, pin)
-onoff(dot, pause, pin)
-onoff(dot, pause + 0.5, pin)
+letter('...')
+letter('---')
+letter('...')
 
 cleanup()
