c62341655a383236a4276c637e096404a4e5e374
Andy Zimolzak (from Raspberry Pi) <andyzimolzak@gmail.com>
2015-11-21 19:10:59 -0500

    tidy up

--- blinken.py
+++ blinken.py
@@ -1,15 +1,11 @@
 #!/usr/bin/env python
 
 from strobe import letter, setup, cleanup
-from time import sleep
 
 pin = 11
-
 setup(pin)
-
 for i in range(2):
     letter('-.--', pin)
     letter('-.--', pin)
     letter('--..', pin)
-
 cleanup()
