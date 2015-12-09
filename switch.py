2287f6adfec8528416b6a2b216f927b74b8187de
Andy Zimolzak (from Raspberry Pi) <andyzimolzak@gmail.com>
2015-12-08 22:24:23 -0500

    Echo what user does

--- switch.py
+++ switch.py
@@ -3,16 +3,33 @@
 import RPi.GPIO as GPIO
 from time import sleep
 
-pin = 7
+pin_switch = 7
+pin_led = 22
 GPIO.setmode(GPIO.BOARD) 
-GPIO.setup(pin, GPIO.IN) # requires root
+GPIO.setup(pin_switch, GPIO.IN) # requires root
+GPIO.setup(pin_led, GPIO.OUT) # requires root
+GPIO.output(pin_led, GPIO.LOW)
+
+period = 0.33
+
+
+series = []
 
 for i in range(24):
-    z = GPIO.input(pin)
+    z = GPIO.input(pin_switch)
     if z:
-        print "high"
+        print "not pushed"
+        series.append(0)
     else:
-        print "low"
-    sleep(0.33)
+        print "pushed"
+        series.append(1)
+    sleep(period)
+
+for v in series:
+    if v:
+        GPIO.output(pin_led, GPIO.HIGH)
+    else:
+        GPIO.output(pin_led, GPIO.LOW)
+    sleep(period)
 
 GPIO.cleanup()
