064e263f922352f96230774adf5898f85964a5d0
Andy Zimolzak (from Raspberry Pi) <andyzimolzak@gmail.com>
2015-12-11 19:31:25 -0500

    Pulsate at a certain period.

--- duty_cycle.py
+++ duty_cycle.py
@@ -10,9 +10,13 @@ up = range(10)
 down = range(9)
 down.reverse()
 
+period = 1.0 # seconds
+
+time_per_level = period / len(up + down)
+
 for i in range(10):
     for j in (up + down):
         brightness = (j+1) / 10.0
-        dimmer(brightness, 0.1, pin)
+        dimmer(brightness, time_per_level, pin)
 
 cleanup()
