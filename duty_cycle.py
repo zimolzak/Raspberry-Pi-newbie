e055874545dcc0e1205bad2b419076c204ffcf9c
Andy Zimolzak (from Raspberry Pi) <andyzimolzak@gmail.com>
2015-12-12 08:51:26 -0500

    Add a 0.0 duty cycle: brief moment of off time.

--- duty_cycle.py
+++ duty_cycle.py
@@ -3,21 +3,20 @@
 from blinkenlights import dimmer, setup, cleanup
 
 pin = 18
+bpm = 70
 
 setup(pin)
 
 up = range(10)
 down = range(9)
 down.reverse()
-
-bpm = 70
+spectrum = up + down + [-1]
 
 period = 60.0 / bpm # seconds
-
-time_per_level = period / len(up + down)
+time_per_level = period / len(spectrum)
 
 for i in range(10):
-    for j in (up + down):
+    for j in (spectrum):
         brightness = (j+1) / 10.0
         dimmer(brightness, time_per_level, pin)
 
