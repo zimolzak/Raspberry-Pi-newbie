85b5e9ea9e9119ac9526f250dae1466b6dc1145b
Andy Zimolzak (from Raspberry Pi) <andyzimolzak@gmail.com>
2015-12-18 22:13:38 -0500

    Randomly varying numbers and function to encode them.

--- fake_meter.py
+++ fake_meter.py
@@ -2,17 +2,30 @@
 
 import RPi.GPIO as GPIO
 from time import sleep
+from random import randint
 
 pins = [26, 6, 13, 22]
+npins = len(pins)
 
 GPIO.setmode(GPIO.BCM)  # GPIO.BOARD
 GPIO.setup(pins, GPIO.OUT) # requires root
 
-for p in pins:
-    GPIO.output(p, GPIO.LOW)
+def display(value):
+    assert 0 <= value <= npins
+    for p in pins:
+        GPIO.output(p, GPIO.LOW) # reset them all
+    for p in pins[:value]:
+        GPIO.output(p, GPIO.HIGH) # turn on the few
 
-for p in pins:
-    GPIO.output(p, GPIO.HIGH)
-    sleep(1)
+x = 2
+
+while(True):
+    x = x + randint(-1,1)
+    if x > npins:
+        x = npins
+    if x < 0:
+        x = 0
+    display(x)
+    sleep(0.2)
 
 GPIO.cleanup()
