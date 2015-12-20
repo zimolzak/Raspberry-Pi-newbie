f6e8f1e98b1c09bfdb684b46c51266b4b6a1026c
Andy Zimolzak (from Raspberry Pi) <andyzimolzak@gmail.com>
2015-12-20 09:26:44 -0500

    Reformat the dictionary line of code.

--- seven_segment.py
+++ seven_segment.py
@@ -20,14 +20,13 @@ def light_segments(segstring):
         GPIO.output(pins[int(c)], GPIO.HIGH)
 
 alpha = dict(a='012345', b='13456', c='0146', d='23456', e='01346', f='0134',
-             g='012356', h='14325', i='25',
-             j='2564', k='134', l='146', m='02356', n='435', o='3456',
-             p='01234',
-             q='01235', r='34',
+             g='012356', h='14325', i='25', j='2564', k='134', l='146',
+             m='02356', n='435', o='3456', p='01234', q='01235', r='34',
              s='01356', t='1436', u='14652', v='456', w='123456', x='35',
              y='12356', z='02346')
 
-# not great: kmwvxz
+# not great: k   m  w  v x  z
+#            |-  3  uu u 7  2
 
 try:
     for c in pipe_contents:
@@ -36,7 +35,7 @@ try:
             light_segments(alpha[c])
         else: # no lights for non-letters
             light_segments('')
-        sleep(1)
+        sleep(2)
 except KeyboardInterrupt:
     pass
 
