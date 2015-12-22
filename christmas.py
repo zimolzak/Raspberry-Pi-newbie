dc6532ae5e67db88046f241f8f79076e6f4b3ea4
Andy Zimolzak (from Raspberry Pi) <andyzimolzak@gmail.com>
2015-12-22 12:03:24 -0500

    Full working light show, 64 randomly chosen patterns.

--- christmas.py
+++ christmas.py
@@ -1,7 +1,12 @@
 #!/usr/bin/env python
 
-"""Make four LEDs ascend and descend, at a pleasant variety of
-rates."""
+"""Make some LEDs blink in pleasant, maybe Christmas-like patterns.
+
+Created for Christmas, using rhythms/patterns that remind me of
+jingling sleigh bells. Be sure to use red and green LEDs. If you use 4
+LEDs, there are 64 basic pattern possibilities (2**4 LED * 2 patterns
+* 2 tempi).
+"""
 
 from blinkenlights import setup, cleanup
 from fourleds import light, clear, encode
@@ -9,15 +14,12 @@ from time import sleep
 from random import randint
 
 pins = [32, 22, 18, 16]
-#       blu grn red yel
-
 setup(pins)
-
-t0 = 0.4
+t0 = 0.4 # Basic time unit for the rhythm
+xmax = (2 ** len(pins)) - 1
 
 def abab(x, t):
     assert type(x) == int
-    xmax = (2 ** len(pins)) - 1
     assert 0 <= x <= xmax
     for i in range(4):
         encode(x, pins)
@@ -27,9 +29,8 @@ def abab(x, t):
 
 def aabb(x, t):
     assert type(x) == int
-    xmax = (2 ** len(pins)) - 1
     assert 0 <= x <= xmax
-    k = 0.7 # Defines how legato vs stacatto
+    k = 0.7 # Defines how legato vs staccato
     for i in range(2):
         for j in range(2):
             encode(x, pins)
@@ -43,8 +44,12 @@ def aabb(x, t):
             sleep(t * (1-k))
 
 try:
-    abab(5, t0)
-    aabb(5, t0)
+    while(True):
+        time_factor = randint(1,2)
+        if randint(0,1):
+            abab(randint(0,xmax), t0 * time_factor)
+        else:
+            aabb(randint(0,xmax), t0 * time_factor)
 except KeyboardInterrupt:
     pass
 
