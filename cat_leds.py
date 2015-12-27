6f17f131edba016d29cf59937382c55d3410f0f6
Andy Zimolzak (from Raspberry Pi) <andyzimolzak@gmail.com>
2015-12-27 11:25:20 -0500

    fix comment

--- cat_leds.py
+++ cat_leds.py
@@ -6,7 +6,7 @@ import sys
 import RPi.GPIO as GPIO
 from seven_segment import print_leds
 
-pins = [17, 23, 24, 22, 27, 25, 5] ## FIXME - annoying and bad, in 2 places
+pins = [17, 23, 24, 22, 27, 25, 5]
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(pins, GPIO.OUT)
 
