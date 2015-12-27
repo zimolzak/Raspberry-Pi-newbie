b0dba4ace9a9b3fd2e74292cdd7cce9b9d98d2fd
Andy Zimolzak (from Raspberry Pi) <andyzimolzak@gmail.com>
2015-12-27 11:45:59 -0500

    Improve display of + and X.

--- seven_segment.py
+++ seven_segment.py
@@ -14,7 +14,7 @@ def light_segments(segstring, pins):
 alpha = dict(a='012345', b='13456', c='0146', d='23456', e='01346', f='0134',
              g='012356', h='14325', i='25', j='2564', k='134', l='146',
              m='02356', n='435', o='3456', p='01234', q='01235', r='34',
-             s='01356', t='1436', u='14652', v='456', w='123456', x='35',
+             s='01356', t='1436', u='14652', v='456', w='123456', x='1245',
              y='12356', z='02346')
 
 alpha[','] = '56'
@@ -40,11 +40,10 @@ alpha['8'] = '0123456'
 alpha['9'] = '012356'
 alpha['7'] = '025'
 alpha['0'] = '012456'
+alpha['+'] = '235'
 
 # not great: k   m  w  v x  z
 #            |-  3  uu u 7  2
-
-# FIXME - add the + character
 
 def print_leds(string, pins, delay=2):
     assert type(string) == str
