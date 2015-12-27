6d7d02f2f2d8e203f8c6c3e73156a68b71dbd25e
Andy Zimolzak (from Raspberry Pi) <andyzimolzak@gmail.com>
2015-12-27 15:15:19 -0500

    Quite good dark room settings.

--- camera.py
+++ camera.py
@@ -4,13 +4,21 @@
 
 import picamera
 import sys
+from fractions import Fraction
+from time import sleep
 
 filename = sys.argv[1]
 camera = picamera.PiCamera()
 camera.vflip = True
 camera.hflip = True
-camera.brightness = 65
-#camera.contrast = 20
+
+camera.framerate = Fraction(1, 6)
+camera.shutter_speed = 4000000
+#camera.exposure_mode = 'off'
+camera.iso = 1600
+print "sleep"
+sleep(4)
+print "go"
 
 # camera.sharpness = 0
 # camera.contrast = 0
@@ -19,7 +27,6 @@ camera.brightness = 65
 # camera.ISO = 0
 # camera.video_stabilization = False
 # camera.exposure_compensation = 0
-camera.exposure_compensation = 6
 # camera.exposure_mode = 'auto'
 # camera.meter_mode = 'average'
 # camera.awb_mode = 'auto'
