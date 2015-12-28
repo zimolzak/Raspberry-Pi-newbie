949a29f1175e49e2a663f698ccc9543af1d30ce9
Andy Zimolzak (from Raspberry Pi) <andyzimolzak@gmail.com>
2015-12-27 19:18:47 -0500

    Use temperature sensor to control some LEDs.

--- temperature.py
+++ temperature.py
@@ -6,15 +6,7 @@
 
 import glob
 from time import sleep
-
-period = 0.3333
-duration = 8
-samples = int(duration / float(period))
-freq = 1.0 / period
-
-series = []
-
-print "inputting", samples, "samples,", "at", freq, "Hz"
+import RPi.GPIO as GPIO
 
 ####
 
@@ -22,25 +14,34 @@ base_dir = '/sys/bus/w1/devices/'
 device_folder = glob.glob(base_dir + '28*')[0]
 device_file = device_folder + '/w1_slave'
 
-lines = open(device_file, 'r').readlines()
-
-t = int(lines[1][-6:])
-
-print t
-
-temp_c = t / 1000.0
-temp_f = temp_c * 9.0 / 5.0 + 32.0
-print temp_c, temp_f
-
-quit()
-
 ####
 
+pins = [24, 23, 18]
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(pins, GPIO.OUT)
 
+def light_only(x):
+    for p in pins:
+        GPIO.output(p, GPIO.LOW) # reset the many
+    GPIO.output(x, GPIO.HIGH)
 
-for i in range(samples):
-    series.append((GPIO.input(pin_sense)) ^ 1)
-    print series[i]
-    sleep(period)
+def represent(temperature):
+    if temperature >= 85:
+        light_only(18)
+    elif 72 < temperature < 85:
+        light_only(23)
+    else:
+        light_only(24)
 
+try:
+    while True:
+        lines = open(device_file, 'r').readlines()
+        t = int(lines[1][-6:])
+        temp_c = t / 1000.0
+        temp_f = temp_c * 9.0 / 5.0 + 32.0
+        represent(temp_f)
+        sleep(1)
+except KeyboardInterrupt:
+    pass
 
+GPIO.cleanup()
