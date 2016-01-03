b4281abe6e9fb7fad7e725f6b3f3c6c8007083cd
Andy Zimolzak (from Raspberry Pi) <andyzimolzak@gmail.com>
2016-01-03 13:38:42 -0500

    Camera changes for jigsaw puzzle timelapse: resolution, quality, filename.

--- repetition_cam.py
+++ repetition_cam.py
@@ -10,10 +10,11 @@ filename = sys.argv[1]
 camera = picamera.PiCamera()
 camera.vflip = True
 camera.hflip = True
+camera.resolution = (1296, 972) #exactly half
 
 try:
     while True:
-        sleep(0.5)
-        camera.capture(filename)
+        sleep(2)
+        camera.capture(filename, quality=50)
 except KeyboardInterrupt:
     pass
--- timelapse.py
+++ timelapse.py
@@ -7,20 +7,22 @@
 import picamera
 import time
 
-base_file_name = "dad"
+base_file_name = "puzzle"
 
 camera = picamera.PiCamera()
 camera.vflip = True
 camera.hflip = True
+camera.resolution = (1296, 972) #exactly half
 
-delay = 1
-minutes = 10 / 60.0
+delay = 60 # seconds
+minutes = 120
 samples = int(minutes * 60.0 / delay)
 
 for i in range(samples):
     time.sleep(delay)
+    t_hr = ''.join(map(str,time.localtime()[3:4])).zfill(2)
     t_min = ''.join(map(str,time.localtime()[4:5])).zfill(2)
     t_sec = ''.join(map(str,time.localtime()[5:6])).zfill(2)
-    filename = base_file_name + t_min + t_sec + '.jpg'
-    camera.capture(filename)
+    filename = base_file_name + t_hr + t_min + t_sec + '.jpg'
     print filename
+    camera.capture(filename, quality=50)
