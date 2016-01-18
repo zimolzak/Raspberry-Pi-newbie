82c31412190e42f98ce65d5ad1a6a9b8faad2cb6
Andy Zimolzak (from Raspberry Pi) <andyzimolzak@gmail.com>
2016-01-18 12:14:50 -0500

    Handle when N/A comes thru the quote.

--- lcd_ticker.py
+++ lcd_ticker.py
@@ -10,16 +10,31 @@ symbols = ['AAPL', 'MSFT', 'F', 'T', 'KO', 'GOOG', 'SYK', 'DIS', 'GM', 'GE',
            'RTN']
 
 def compact_quote(symbol):
-    symbol = 'SYK'
     a = y.get_all(symbol)
-
-    L52 = int(round(float(a['fifty_two_week_low']), 0))
-    P = round(float(a['price']), 1)
-    C = a['change']
-    H52 = int(round(float(a['fifty_two_week_high']), 0))
-    PE = round(float(a['price_earnings_ratio']), 1)
-    Cp = int(round(float(C) / float(P) * 100))
-
+    try:
+        L52 = int(round(float(a['fifty_two_week_low']), 0))
+    except ValueError:
+        L52 = '_'
+    try:
+        P = round(float(a['price']), 1)
+    except ValueError:
+        P = '_'
+    try:
+        C = a['change']
+    except ValueError:
+        C = '_'
+    try:
+        H52 = int(round(float(a['fifty_two_week_high']), 0))
+    except ValueError:
+        H52 = '_'
+    try:
+        PE = round(float(a['price_earnings_ratio']), 1)
+    except ValueError:
+        PE = '_'
+    try:
+        Cp = int(round(float(C) / float(P) * 100))
+    except ValueError:
+        Cp = '_'
     return '{} {} {}% [{} {}] PE {}'.format(symbol, P, Cp, L52, H52, PE)
 
 while(True):
