d5367b165a43881e336f877dd74911975efbd6e9
Andrew Zimolzak <andyzimolzak@gmail.com>
2016-01-18 11:50:13 -0500

    Working function for compact quote.

--- test.py
+++ test.py
@@ -1,16 +1,17 @@
 #!/usr/bin/env python
 import ystockquote as y
 
-x = 'SYK'
-a = y.get_all(x)
+def compact_quote(symbol):
+    symbol = 'SYK'
+    a = y.get_all(symbol)
 
-L52 = int(round(float(a['fifty_two_week_low']), 0))
-P = round(float(a['price']), 1)
-C = a['change']
-H52 = int(round(float(a['fifty_two_week_high']), 0))
-PE = round(float(a['price_earnings_ratio']), 1)
-Cp = int(round(float(C) / float(P) * 100))
+    L52 = int(round(float(a['fifty_two_week_low']), 0))
+    P = round(float(a['price']), 1)
+    C = a['change']
+    H52 = int(round(float(a['fifty_two_week_high']), 0))
+    PE = round(float(a['price_earnings_ratio']), 1)
+    Cp = int(round(float(C) / float(P) * 100))
 
-printme = '{} {} {}% [{} {}] PE {}'.format(x, P, Cp, L52, H52, PE)
+    return '{} {} {}% [{} {}] PE {}'.format(symbol, P, Cp, L52, H52, PE)
 
-print printme
+print compact_quote('YHOO')
