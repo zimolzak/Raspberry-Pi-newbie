458688aa3a1ce901b2ffcf64497965484cea4e53
Andy Zimolzak (from Raspberry Pi) <andyzimolzak@gmail.com>
2016-01-22 00:40:03 -0500

    Update the DB-inserting script for wifi.

--- temperature_db.py
+++ temperature_db.py
@@ -23,10 +23,10 @@ try:
         data['temperature'] = str(temp_f)
         data['room'] = '1'
         url_values = urllib.urlencode(data)
-        url = 'http://192.168.1.6/addtemperature'
+        url = 'http://192.168.1.4/addtemperature'
         full_url = url + '?' + url_values
         data = urllib2.urlopen(full_url)
         print data.read()
-        sleep(4)
+        sleep(60)
 except KeyboardInterrupt:
     pass
