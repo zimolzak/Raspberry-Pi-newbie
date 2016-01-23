26d894b857dd82b1de92e50abca310fec7306924
Andrew Zimolzak <andyzimolzak@gmail.com>
2016-01-22 23:25:37 -0500

    Just a single plot, better x label.

--- analyze_temperature.R
+++ analyze_temperature.R
@@ -1,17 +1,10 @@
 # http://www.r-bloggers.com/using-sqlite-in-r/
 	
 library("RSQLite")
-con = dbConnect(RSQLite::SQLite(), "~/Desktop/temperature.db")
-p1 = dbGetQuery(con, 'select * from temperature where id > 1000')
-p2 = dbGetQuery(con, 'select count(*) from temperature')
-p3 = dbGetQuery(con, 'select * from roomdetails')
-p4 = dbGetQuery(con, 'select * from temperature where roomid > 1')
-
 library(ggplot2)
-qplot(datetime, temperature, data=p4, colour=as.factor(roomid))
+
+con = dbConnect(RSQLite::SQLite(), "~/Desktop/temperature.db")
 
 joined = dbGetQuery(con, 'select * from temperature left join roomdetails on roomdetails.id = temperature.roomid where roomid > 1')
 
-qplot(datetime, temperature, data=joined, colour=as.factor(room))
-
-qplot(strptime(datetime, '%Y-%m-%d %H:%M:%S'), temperature, data=joined, colour=as.factor(room))
+qplot(strptime(datetime, '%Y-%m-%d %H:%M:%S'), temperature, data=joined, colour=as.factor(room), xlab="Time")
