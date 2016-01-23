cc1d079643cc8e01da574803caca6463e5d5afd8
Andrew Zimolzak <andyzimolzak@gmail.com>
2016-01-23 15:28:24 -0500

    Split into 3 plots.

--- make_web_plots.R
+++ make_web_plots.R
@@ -1,20 +1,24 @@
 #!/usr/bin/env Rscript
-
 library("RSQLite")
 library(ggplot2)
 
-con = dbConnect(RSQLite::SQLite(), "~/Desktop/local/Raspberry-Pi-newbie/temperature.db")
+con = dbConnect(RSQLite::SQLite(), "~/Desktop/local/Raspberry-Pi-newbie/temperature.db") # localize
 
 joined = dbGetQuery(con, 'select * from temperature left join roomdetails on roomdetails.id = temperature.roomid where roomid > 1')
-
 fixdate = cbind(joined, data.frame(posix_t = strptime(joined$datetime, '%Y-%m-%d %H:%M:%S', tz="GMT")))
 
 k = 6
-
 horizon = Sys.time() - 3600 * k
-
 last_k_hours = fixdate[fixdate$posix_t > horizon,]
+radiator = last_k_hours[last_k_hours$roomid == 2,]
+catbed = last_k_hours[last_k_hours$roomid == 3,]
+outside = last_k_hours[last_k_hours$roomid == 4,]
 
-p = qplot(posix_t, temperature, data=last_k_hours, colour=as.factor(room), xlab="Time (UTC)")
+R = qplot(posix_t, temperature, data=radiator, xlab="Time (UTC)", main="Radiator")
+C = qplot(posix_t, temperature, data=catbed, xlab="Time (UTC)", main="Cat bed")
+O = qplot(posix_t, temperature, data=outside, xlab="Time (UTC)", main="Outdoors")
 
-ggsave(p, file="~/Desktop/last6.png")
+# localize all of these to WWW
+ggsave(R, file="~/Desktop/radiator.png")
+ggsave(C, file="~/Desktop/catbed.png")
+ggsave(O, file="~/Desktop/outside.png")
