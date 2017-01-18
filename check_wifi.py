f550c290bdb4778ff221aeafdd889416186c9cae
Andy Zimolzak (from Raspberry Pi) <andyzimolzak@gmail.com>
2017-01-17 23:04:04 -0500

    Logging turns out to be a neat thing!

--- check_wifi.py
+++ check_wifi.py
@@ -1,22 +1,25 @@
 ## usage:
-##    python3 check_wifi.py
+##    python3 check_wifi.py &
 
 from subprocess import check_output, CalledProcessError
 import re
 import time
+import logging
+
+logging.basicConfig(filename='../wlan_resets.log',level=logging.INFO)
 
 ips_to_ping = ['8.8.8.8', '192.168.1.1']
 sleep_seconds = 15 * 60
 
 while(True):
-    print(time.strftime("%Y-%m-%d %H:%M:%S") + " --------")
+    logging.info(time.strftime("%Y-%m-%d %H:%M:%S") + " --------")
     commands = ['ping -c 4 ' + ip for ip in ips_to_ping]
     try:
         outs = [check_output(cmd.split()) for cmd in commands]
     except CalledProcessError:
-        print('one or both pings returned error, poss because 0 packets')
+        logging.info('one or both pings returned error, poss because 0 packets')
         output = check_output(['wpa_cli', 'reassociate'])
-        print('wpa_cli says ' + str(output))
+        logging.info('wpa_cli says ' + str(output))
         time.sleep(sleep_seconds)
         continue
     for j, b in enumerate(outs):
@@ -25,5 +28,5 @@ while(True):
                 s = str(line)
                 search = re.search(r'(\d+)% packet loss', str(b))
                 n = int(search.group(1)) # 1st parenth'd subgroup
-                print(ips_to_ping[j], '\t', n, 'loss')
+                logging.info(ips_to_ping[j] + '\t' + str(n) + ' loss')
     time.sleep(sleep_seconds)
