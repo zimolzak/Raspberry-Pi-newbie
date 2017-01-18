37793fa47127667d298aebd7636b8d7e20614ca1
Andy Zimolzak (from Raspberry Pi) <andyzimolzak@gmail.com>
2017-01-17 22:08:26 -0500

    Big enclosing loop

--- check_wifi.py
+++ check_wifi.py
@@ -1,19 +1,25 @@
 from subprocess import check_output, CalledProcessError
 import re
+import time
 
-ping8 = "ping -c 4 8.8.8.8"
-ping1 = "ping -c 4 192.168.1.1"
+ips_to_ping = ['8.8.8.8', '192.168.1.1']
+sleep_seconds = 15 * 60
 
-try:
-    outs = [check_output(x.split()) for x in [ping8, ping1]]
-except CalledProcessError:
-    print('one or both pings returned error, poss because 0 packets')
-    quit()
-
-for b in outs:
-    for line in b.splitlines():
-        if 'packet loss' in str(line):
-            s = str(line)
-            search = re.search(r'(\d+)% packet loss', str(b))
-            n = int(search.group(1)) # 1st parenth'd subgroup
-            print(n)
+while(True):
+    print(time.strftime("%Y-%m-%d %H:%M:%S") + " --------")
+    commands = ['ping -c 4 ' + ip for ip in ips_to_ping]
+    try:
+        outs = [check_output(cmd.split()) for cmd in commands]
+    except CalledProcessError:
+        print('one or both pings returned error, poss because 0 packets')
+        ### wpa_cli reassociate
+        time.sleep(sleep_seconds)
+        continue
+    for j, b in enumerate(outs):
+        for line in b.splitlines():
+            if 'packet loss' in str(line):
+                s = str(line)
+                search = re.search(r'(\d+)% packet loss', str(b))
+                n = int(search.group(1)) # 1st parenth'd subgroup
+                print(ips_to_ping[j], '\t', n, 'loss')
+    time.sleep(sleep_seconds)
