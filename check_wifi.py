25afa92554b3fede5377106bacb20efdfe8fffd3
Andy Zimolzak (from Raspberry Pi) <andyzimolzak@gmail.com>
2017-01-20 08:52:40 -0500

    Change "print" to "logging.info" - should be good to go.

--- check_wifi.py
+++ check_wifi.py
@@ -8,21 +8,27 @@ import logging
 
 logging.basicConfig(filename='../wlan_resets.log',level=logging.INFO)
 
-ips_to_ping = ['8.8.8.8', '192.168.1.66']
+ips_to_ping = ['8.8.8.8', '192.168.1.1']
 sleep_seconds = 15 * 60
 
 def print_exception(e):
-    print('object\t', e)
-    print('type\t', type(e))
+    logging.info('object\t' + str(e))
+    logging.info('type\t' + str(type(e)))
     if type(e) == CalledProcessError:
-        print('args\t', e.args)
-        print('cmd\t' , e.cmd)
-        print('output\t', e.output.decode('utf-8').replace('\n','\\n'))
-        print('r-code\t', e.returncode)
-        print('stderr\t' , e.stderr)
-        print('stdout\t' , e.stdout)
+        logging.info('args  \t' + str(e.args))
+        logging.info('cmd   \t'  + str(e.cmd))
+        logging.info('output\t' + e.output.decode('utf-8').replace('\n','\\n'))
+        logging.info('r-code\t' + str(e.returncode))
+        if hasattr(e, 'stderr'):
+            logging.info('stderr\t'  + str(e.stderr))
+        if hasattr(e, 'stdout'):
+            logging.info('stdout\t'  + str(e.stdout))
+        if hasattr(e, 'message'):
+            logging.info('message\t'  + str(e.message))
+        if (not hasattr(e, 'stderr')) and (not hasattr(e, 'stdout')):
+            logging.info(str(dir(e)))
     else:
-        print(dir(e))
+        logging.info(str(dir(e)))
 
 while(True):
     logging.info(time.strftime("%Y-%m-%d %H:%M:%S") + " --------")
@@ -32,10 +38,12 @@ while(True):
     except CalledProcessError:
         logging.info('>= 1 ping returned error, poss because 0 packets')
         try:
-            output = check_output(['wpa_cli', 'reassociatexxx'], stderr=STDOUT)
+            output = check_output(['wpa_cli', 'reassociate'], stderr=STDOUT)
         except Exception as e:
             print_exception(e)
-        logging.info('wpa_cli says ' + str(output))
+        else:
+            logging.info('wpa_cli success!!')
+            logging.info('wpa_cli says ' + str(output))
         time.sleep(sleep_seconds)
         continue
     for j, b in enumerate(outs):
