8bbc78d733024cdc099b594c8a9dcb3353d356d6
Andy Zimolzak (from Raspberry Pi) <andyzimolzak@gmail.com>
2017-01-20 08:33:45 -0500

    First try at inspecting wpa_cli behavior.

--- check_wifi.py
+++ check_wifi.py
@@ -1,15 +1,28 @@
 ## usage:
 ##    python3 check_wifi.py &
 
-from subprocess import check_output, CalledProcessError
+from subprocess import check_output, CalledProcessError, STDOUT
 import re
 import time
 import logging
 
 logging.basicConfig(filename='../wlan_resets.log',level=logging.INFO)
 
-ips_to_ping = ['8.8.8.8', '192.168.1.1']
+ips_to_ping = ['8.8.8.8', '192.168.1.66']
 sleep_seconds = 15 * 60
+
+def print_exception(e):
+    print('object\t', e)
+    print('type\t', type(e))
+    if type(e) == CalledProcessError:
+        print('args\t', e.args)
+        print('cmd\t' , e.cmd)
+        print('output\t', e.output.decode('utf-8').replace('\n','\\n'))
+        print('r-code\t', e.returncode)
+        print('stderr\t' , e.stderr)
+        print('stdout\t' , e.stdout)
+    else:
+        print(dir(e))
 
 while(True):
     logging.info(time.strftime("%Y-%m-%d %H:%M:%S") + " --------")
@@ -17,8 +30,11 @@ while(True):
     try:
         outs = [check_output(cmd.split()) for cmd in commands]
     except CalledProcessError:
-        logging.info('one or both pings returned error, poss because 0 packets')
-        output = check_output(['wpa_cli', 'reassociate'])
+        logging.info('>= 1 ping returned error, poss because 0 packets')
+        try:
+            output = check_output(['wpa_cli', 'reassociatexxx'], stderr=STDOUT)
+        except Exception as e:
+            print_exception(e)
         logging.info('wpa_cli says ' + str(output))
         time.sleep(sleep_seconds)
         continue
