cdd00fc4a960e035e415cb7d03b7b48b2179f80f
Andy Zimolzak (from Raspberry Pi) <andyzimolzak@gmail.com>
2018-03-05 17:00:55 -0500

    Change a lot of settings back to default. Fixes grey screen problem that came up.

--- repeating_camera.py
+++ repeating_camera.py
@@ -31,16 +31,20 @@ def setup_cam():
     # Usually hflip = True. But "hflip = False" gives the desired mirror
     # effect if you're facing your camera and facing your browser aka
     # laptop screen.
+    #
+    #camera.vflip = True
+    #camera.hflip = True
+    #camera.rotation = 180
+    ## camera.framerate = Fraction(1, 6)
+    #duration_sec = 1
+    #duration_microsec = int(duration_sec * 1000000)
+    #camera.shutter_speed = duration_microsec
+    #camera.exposure_mode = 'auto' # camera.exposure_mode = 'off'
+    #camera.iso = 800
     camera = picamera.PiCamera()
-    camera.vflip = True
-    camera.hflip = True
-    camera.rotation = 180
     camera.resolution = (432, 324) #exactly sixth. Full = 2592, 1944
-    camera.framerate = Fraction(1, 6)
-    sec = 1
-    camera.shutter_speed = int(sec * 1000000)
-    camera.exposure_mode = 'off'
-    camera.iso = 800
+    camera.start_preview()
+    sleep(2)
     return camera
 
 CAM = setup_cam()
@@ -53,6 +57,7 @@ try:
 #        s = dt.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 #        CAM.annotate_text = "duh"
         CAM.capture("temp.jpg", quality=40)
+        sleep(0.5)
         shutil.copy("temp.jpg", FILENAME)
         current = dt.datetime.now().hour
         if current > stored or (current == 0 and stored == 23):
@@ -60,4 +65,4 @@ try:
             shutil.copy("temp.jpg", 'hour'+str(current)+'.jpg')
 
 except KeyboardInterrupt:
-    pass
+    camera.close()
